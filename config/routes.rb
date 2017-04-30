b50c024d5f485b1aa5e29da63bc72976392919cd
thomas birien <thomasbirien@gmail.com>
2017-04-30 16:24:11 +0200

    new controller for profile created

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
-  get 'item/index'
 
-  get 'item/show'
-
-  get 'item/new'
-
-  get 'item/create'
-
-  get 'item/edit'
-
-  get 'item/update'
-
-  get 'item/destroy'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
