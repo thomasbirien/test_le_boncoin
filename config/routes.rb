da920da68a5d2bfdfe04992a70e72ffe35be15bb
thomas birien <thomasbirien@gmail.com>
2017-04-30 16:22:06 +0200

    new controller for Item

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'item/index'
+
+  get 'item/show'
+
+  get 'item/new'
+
+  get 'item/create'
+
+  get 'item/edit'
+
+  get 'item/update'
+
+  get 'item/destroy'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
